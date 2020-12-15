36cf3bf5f793e01063ec45390da6834c55e45b25
Zaid <zaid.naom@gmail.com>
2020-12-15 08:24:22 +0100

    Feliz integration

---
+++ samples/FelizSvelte/src/Main.fs
@@ -0,0 +1,6 @@
+﻿module Main
+
+open Feliz
+open Browser.Dom
+
+ReactDOM.render(App.Application(), document.body)
---
+++ samples/FelizSvelte/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+const mode = process.env.NODE_ENV || 'development';
+const prod = mode === 'production';
+
+module.exports = {
+	entry: {
+		bundle: ['./src/Main.fs.js']
+	},
+	resolve: {
+		alias: {
+			svelte: path.resolve('node_modules', 'svelte')
+		},
+		extensions: ['.fs.js', '.mjs', '.js', '.svelte'],
+		mainFields: ['svelte', 'browser', 'module', 'main']
+	},
+	output: {
+		path: __dirname + '/public',
+		filename: '[name].js',
+		chunkFilename: '[name].[id].js',
+	},
+	// devServer: {
+	//     public: '/',
+	// 	   contentBase: __dirname + '/public',
+	//     port: 8080,
+	//     host: '0.0.0.0',
+	//     // hot: true,
+	//     // inline: true
+	// },
+	module: {
+		rules: [
+			{
+				test: /\.svelte$/,
+				use: {
+					loader: 'svelte-loader',
+					options: {
+						emitCss: prod,
+						hotReload: !prod,
+						dev: !prod,
+					}
+				}
+			}
+		]
+	},
+	mode,
+	devtool: prod ? false : 'source-map'
+};
