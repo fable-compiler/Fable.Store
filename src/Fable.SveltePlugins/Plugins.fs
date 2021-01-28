a2d040e5be24e35736c6c327aa41ef16c59666b3
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2021-01-29 03:45:57 +0900

    Start work to generate d.ts

--- src/Fable.SveltePlugins/Plugins.fs
+++ src/Fable.SveltePlugins/Plugins.fs
@@ -6,6 +6,46 @@ open Fable.AST
 do()
 
 type Helper =
+    static member PrintType(h: Fable.PluginHelper, i: int, t: Fable.Type) =
+        let rec nestedLambda acc = function
+            | Fable.LambdaType(arg, returnType) ->
+                nestedLambda (arg::acc) returnType
+            | returnType -> List.rev acc, returnType
+
+        let printArg (i: int) arg = $"a{i}: {Helper.PrintType(h, i, arg)}"
+        let printFn args t = $"""(({args |> List.mapi printArg |> String.concat ", "}) => {Helper.PrintType(h, i, t)}"""
+        let printRecord fields =
+            [
+                "{"
+                for (name, t) in fields do
+                    $"""{String.replicate 4 " "}{name}: {Helper.PrintType(h, i + 1, t)},"""
+                "}"
+            ] |> String.concat System.Environment.NewLine
+
+        match t with
+        | Fable.Any -> "any"
+        | Fable.Unit -> "undefined"
+        | Fable.Boolean -> "boolean"
+        | Fable.Char
+        | Fable.String -> "string"
+        | Fable.Regex -> "RegExp"
+        | Fable.Enum _
+        | Fable.Number _ -> "number"
+        | Fable.Option t -> $"({Helper.PrintType(h, i, t)}|undefined)"
+        | Fable.Tuple ts -> $"""[{ts |> List.map (fun t -> Helper.PrintType(h, i, t)) |> String.concat ", "}]"""
+        | Fable.Array t -> Helper.PrintType(h, i, t) + "[]"
+        | Fable.List t -> $"Iterable<{Helper.PrintType(h, i, t)}>"
+        | Fable.DelegateType(args, ret) -> printFn args ret
+        | Fable.LambdaType(arg, ret) -> nestedLambda [arg] ret ||> printFn
+        | Fable.GenericParam name -> "any" // name // TODO: print generic args in function/type
+        | Fable.DeclaredType(e, _genArgs) ->
+            let e = h.GetEntity(e)
+            if e.IsFSharpRecord then
+                e.FSharpFields |> List.map (fun f -> f.Name, f.FieldType) |> printRecord
+            else "any"
+        | Fable.AnonymousRecordType(fieldNames, genArgs) -> List.zip (List.ofArray fieldNames) genArgs |> printRecord
+        | Fable.MetaType -> "any" // failwith "System.Type is not supported"
+
     static member MakeDispatcher(helper: Fable.PluginHelper, dispatchRef, msgEntityRef, msgGenArgs) =
         let lowerFirst (str: string) =
             str.[0].ToString().ToLower() + str.[1..]
@@ -49,7 +89,7 @@ type DispatcherAttribute() =
 
                     if bindDispatch then Fable.Let(dispatchRef, dispatch, dispatcher)
                     else dispatcher
-                | _ -> fail()                
+                | _ -> fail()
             | _ -> fail()
         | _ -> fail()
 
