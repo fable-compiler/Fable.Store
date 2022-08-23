60b2bbb823de59f18adb6b51f85294a396181f7e
Jordan Marr <JordanMarr@users.noreply.github.com>
2022-08-23 12:26:36 -0400

    Adds the LitStore `useStore` hook.

---
+++ Fable.LitStore/UseStore.fs
@@ -0,0 +1,37 @@
+﻿module LitStore
+
+open Fable
+open Lit
+open System
+
+type HookContext with
+    member ctx.useStore<'Model>(store: IStore<'Model>) = 
+        
+        // Create placeholder refs with temp defaults (to be replaced with real values from store)
+        let setModelRef = ref(fun (_: 'Model) -> ())
+        let subscriptionRef = ref { new IDisposable with member _.Dispose() = () }
+        
+        // Ensure that store subscription is disposed when component is destroyed
+        ctx.useEffectOnce(fun () -> subscriptionRef.Value)
+
+        let model, setModel = 
+            ctx.useState(fun () ->            
+
+                // Susbscribe
+                let initialModel, subscription = 
+                    Store.subscribeImmediate (fun newModel -> 
+                        setModelRef.Value newModel
+                    ) store 
+
+                // Assign subscription ref (to be disposed with component)                
+                subscriptionRef.Value <- subscription
+
+                initialModel
+            )
+
+        setModelRef.Value <- setModel
+        model
+
+type Hook with
+    static member inline useStore<'Model>(store: IStore<'Model>) = 
+        Hook.getContext().useStore<'Model>(store)
